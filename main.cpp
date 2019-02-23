64c023d79f6cc0163e8785d0fdb698656d52e781
stamilselvan <yestamilselvan@gmail.com>
2019-02-23 18:50:21 +0100

    Merge branch 'DEV-001'

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include "consts.h"
 
 int main() 
 {
-    std::cout << "Hello, World!";
+    std::cout << "\nHello, World! Welcome " + NAME + "\n";
     std::cout << "Added from master";
     return 0;
 }
